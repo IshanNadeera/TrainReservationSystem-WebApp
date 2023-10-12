1470f1bb0ef0f5377257c8948c3cd858052cf163
Ishan Nadeera <IT20016616@my.sliit.lk>
2023-10-12 23:19:05 +0530

    Changed dashboard view

--- dashboard.php
+++ dashboard.php
@@ -29,18 +29,16 @@ if (!isset($_SESSION['id'])) {
             <h1 style="background-color:#000; color:#fff; padding: 10px 15px; border-radius:10px;">Dashboard</h1>
         </div>
 
-        <!-- <div class= "container_area">
-            <button class="add-btn"><a href="add_new_booking.php">Add Booking</a></button>
-        </div> -->
+        <div style="margin-left: 250px; margin-bottom: 50px;">
+            <h1 style="font-size: 65px; margin-top: 50px; color: #007bff; margin-left: 230px;">Railway Reservation System Admin Panel</h1>
+            <h2 style="font-size: 40px; margin-top: 10px; margin-left: 350px;">Efficient Railway Management at Your Fingertips!</h2>
+        </div>
 
         <div>
-            <img src="assets/images/train.jpeg" alt="Image" style="width: 86%; height: 600px; margin-left: 250px;">
+            <img src="assets/images/train.jpeg" alt="Image" style="width: 86.5%; height: 700px; margin-left: 250px;">
         </div>
 
-        <div style="margin-left: 250px;">
-            <h1 style="font-size: 65px; margin-top: 50px; color: #007bff; margin-left: 220px;">Railway Reservation System Admin Panel</h1>
-            <h2 style="font-size: 40px; margin-top: 10px; margin-left: 330px;">Efficient Railway Management at Your Fingertips!</h2>
-        </div>
+        
         
     </div>
     
